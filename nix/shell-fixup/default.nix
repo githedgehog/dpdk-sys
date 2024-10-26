c9cefd152abbc9be6931c1f5ad4ed0313a2725c6
Daniel Noland <daniel.noland@dnoland.cloud>
2024-10-26 09:17:59 -0600

    fixup for build break

--- nix/shell-fixup/default.nix
+++ nix/shell-fixup/default.nix
@@ -5,12 +5,12 @@
   version = "0.0.1";
   src = ./src;
   installPhase = ''
+    chmod 555 $src/etc/pam.d
+    chmod -R 444 $src/etc/pam.d/*
+    chmod 444 $src/etc/group
+    chmod 444 $src/etc/passwd
+    chmod 777 $src/tmp
     cp -a $src $out
-    chmod 555 $out/etc/pam.d
-    chmod -R 444 $out/etc/pam.d/*
-    chmod 444 $out/etc/group
-    chmod 444 $out/etc/passwd
-    chmod 777 $out/tmp
   '';
 
 }
