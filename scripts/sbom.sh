42ed822548c3fd637f35bf48096866ef0b024843
Daniel Noland <daniel@githedgehog.com>
2024-10-31 08:19:09 -0600

    Generate SBOM
    (hacked a bit to reduce build time for testing)

--- scripts/sbom.sh
+++ scripts/sbom.sh
@@ -7,11 +7,14 @@ declare -r sbomnix="github:tiiuae/sbomnix"
 just build-sysroot
 
 declare -r builds="/tmp/dpdk-sys/builds"
+pushd "${builds}"
 declare -r package="env.sysroot"
 
 for libc in "musl64" "gnu64"; do
-  for profile in "debug" "release"; do
-    for dep_type in "runtime" "buildtime"; do
+  # shellcheck disable=SC2043
+  for profile in "release"; do
+    # shellcheck disable=SC2043
+    for dep_type in "runtime"; do
       # shellcheck disable=SC2046,SC2006
       nix run \
         "${sbomnix}#sbomnix" \
@@ -42,15 +45,6 @@ for libc in "musl64" "gnu64"; do
         --verbose=1 \
         $([ "$dep_type" = "buildtime" ] && echo --buildtime) \
         "${builds}/${package}.${libc}.${profile}"
-      # shellcheck disable=SC2046,SC2006
-      nix run \
-        "${sbomnix}#nixgraph" \
-        -- \
-        --out "${builds}/${package}.${libc}.${profile}.${dep_type}.nixgraph.dot" \
-        --depth=99999999 \
-        --verbose=1 \
-        $([ "$dep_type" = "buildtime" ] && echo --buildtime) \
-        "${builds}/${package}.${libc}.${profile}"
     done
     # shellcheck disable=SC2046,SC2006
     nix run \
@@ -59,6 +53,14 @@ for libc in "musl64" "gnu64"; do
       --out "${builds}/${package}.${libc}.${profile}.provenance.json" \
       --verbose=1 \
       --recursive \
+      "${builds}/${package}.${libc}.${profile}"
+    # shellcheck disable=SC2046,SC2006
+    nix run \
+      "${sbomnix}#nixgraph" \
+      -- \
+      --out "${builds}/${package}.${libc}.${profile}.${dep_type}.nixgraph.dot" \
+      --depth=10 \
+      --verbose=1 \
       "${builds}/${package}.${libc}.${profile}"
   done
 done
