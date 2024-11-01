9f59886921efd50974d27fa53ec6b108f1cd569b
Daniel Noland <daniel@githedgehog.com>
2024-11-01 17:45:47 -0600

    drop buildtime and debug search
    It is way too slow

--- scripts/sbom.sh
+++ scripts/sbom.sh
@@ -11,54 +11,42 @@ pushd "${builds}"
 declare -r package="env.sysroot"
 
 for libc in "gnu64" "musl64"; do
-  for profile in "release" "debug"; do
-    for dep_type in "runtime" "buildtime"; do
-      # shellcheck disable=SC2046,SC2006
-      nix run \
-        "${sbomnix}#sbomnix" \
-        -- \
-        --csv "${builds}/${package}.${libc}.${profile}.${dep_type}.sbom.csv" \
-        --cdx "${builds}/${package}.${libc}.${profile}.${dep_type}.sbom.cdx.json" \
-        --spdx "${builds}/${package}.${libc}.${profile}.${dep_type}.sbom.spdx.json" \
-        --verbose=1 \
-        --include-vulns \
-        $([ "$dep_type" = "buildtime" ] && echo --buildtime) \
-        "${builds}/${package}.${libc}.${profile}"
-      # shellcheck disable=SC2046,SC2006
-      nix run \
-        "${sbomnix}#vulnxscan" \
-        -- \
-        --out "${builds}/${package}.${libc}.${profile}.vulns.csv" \
-        --triage \
-        --verbose=1 \
-        $([ "$dep_type" = "buildtime" ] && echo --buildtime) \
-        "${builds}/${package}.${libc}.${profile}"
-      # shellcheck disable=SC2046,SC2006
-      nix run \
-        "${sbomnix}#nix_outdated" \
-        -- \
-        --out "${builds}/${package}.${libc}.${profile}.outdated.csv" \
-        --verbose=1 \
-        $([ "$dep_type" = "buildtime" ] && echo --buildtime) \
-        "${builds}/${package}.${libc}.${profile}"
-    done
-    # shellcheck disable=SC2046,SC2006
-    nix run \
-      "${sbomnix}#provenance" \
-      -- \
-      --out "${builds}/${package}.${libc}.${profile}.provenance.json" \
-      --verbose=1 \
-      --recursive \
-      "${builds}/${package}.${libc}.${profile}"
-    # shellcheck disable=SC2046,SC2006
-    nix run \
-      "${sbomnix}#nixgraph" \
-      -- \
-      --out "${builds}/${package}.${libc}.${profile}.${dep_type}.nixgraph.dot" \
-      --depth=15 \
-      --verbose=1 \
-      "${builds}/${package}.${libc}.${profile}"
-  done
+  nix run \
+    "${sbomnix}#sbomnix" \
+    -- \
+    --csv "${builds}/${package}.${libc}.sbom.csv" \
+    --cdx "${builds}/${package}.${libc}.sbom.cdx.json" \
+    --spdx "${builds}/${package}.${libc}.sbom.spdx.json" \
+    --verbose=1 \
+    --include-vulns \
+    "${builds}/${package}.${libc}.release"
+  nix run \
+    "${sbomnix}#vulnxscan" \
+    -- \
+    --out "${builds}/${package}.${libc}.vulns.csv" \
+    --triage \
+    --verbose=1 \
+    "${builds}/${package}.${libc}.release"
+  nix run \
+    "${sbomnix}#nix_outdated" \
+    -- \
+    --out "${builds}/${package}.${libc}.outdated.csv" \
+    --verbose=1 \
+    "${builds}/${package}.${libc}.release"
+  nix run \
+    "${sbomnix}#provenance" \
+    -- \
+    --out "${builds}/${package}.${libc}.provenance.json" \
+    --verbose=1 \
+    --recursive \
+    "${builds}/${package}.${libc}.release"
+  nix run \
+    "${sbomnix}#nixgraph" \
+    -- \
+    --out "${builds}/${package}.${libc}.nixgraph.dot" \
+    --depth=15 \
+    --verbose=1 \
+    "${builds}/${package}.${libc}.release"
 done
 
 for file in "${builds}/"*.csv; do
