3c0537bbc4fb785170c7fe80a8034f056b1f5376
Daniel Noland <daniel@githedgehog.com>
2024-11-05 14:17:31 -0700

    Clean up CI

--- nix/flags.nix
+++ nix/flags.nix
@@ -1,6 +1,6 @@
 {
   release = rec {
-    CFLAGS="-O3 -ggdb3 -march=x86-64-v4 -mtune=znver4 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
+    CFLAGS="-O3 -ggdb3 -march=znver4 -mtune=znver5 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
     CXXFLAGS=CFLAGS;
     LDFLAGS="-Wl,-O3 -Wl,--gc-sections -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=1 -Wl,-plugin-opt,jobs=1 -Qunused-arguments";
   };
