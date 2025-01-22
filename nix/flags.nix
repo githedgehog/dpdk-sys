66b0aba6a12098caf2e58ab9213849add939592b
Daniel Noland <daniel@githedgehog.com>
2025-01-21 21:33:57 -0700

    Fix build failure due to missing machine config
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/flags.nix
+++ nix/flags.nix
@@ -1,12 +1,13 @@
-{
+rec {
+  machine = "-mcrc32 -mrtm -msse4.2 -mssse3 -march=x86-64-v4 -mtune=znver4";
   release = rec {
-    CFLAGS = "-O3 -ggdb3 -march=x86-64-v4 -mtune=znver5 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
+    CFLAGS = " ${machine} -O3 -ggdb3 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
     CXXFLAGS = CFLAGS;
     LDFLAGS = "-Wl,-O3 -Wl,--gc-sections -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=1 -Wl,-plugin-opt,jobs=1 -Qunused-arguments";
   };
 
   dev = rec {
-    CFLAGS = "-Og -ggdb3 -fno-inline -Qunused-arguments";
+    CFLAGS = " ${machine} -Og -ggdb3 -fno-inline -Qunused-arguments";
     CXXFLAGS = CFLAGS;
     LDFLAGS = "";
   };
