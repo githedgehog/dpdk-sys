8b65c99705350a94e522d64e291be3aadc290496
Daniel Noland <daniel@githedgehog.com>
2025-01-23 13:10:58 -0700

    Fix dpdk-wrapper build
    The build should call ar, cc, and ranlib through variables.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/dpdk-wrapper/default.nix
+++ nix/dpdk-wrapper/default.nix
@@ -19,9 +19,9 @@ stdenv.mkDerivation {
   buildPhase = ''
     set euxo pipefail
     mkdir -p $out/{lib,include}
-    cc $CFLAGS -I${dpdk}/include -I${libbsd.dev}/include -c $src/dpdk_wrapper.c -o wrapper.o;
-    ar rcs $out/lib/libdpdk_wrapper.a wrapper.o;
-    ranlib $out/lib/libdpdk_wrapper.a;
+    $CC $CFLAGS -I${dpdk}/include -I${libbsd.dev}/include -c $src/dpdk_wrapper.c -o wrapper.o;
+    $AR rcs $out/lib/libdpdk_wrapper.a wrapper.o;
+    $RANLIB $out/lib/libdpdk_wrapper.a;
     cp $src/*.h $out/include
   '';
 
