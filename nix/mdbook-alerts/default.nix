11871e38b76dca016a899536c36e6e23a8e04517
Daniel Noland <daniel@githedgehog.com>
2025-01-24 18:36:03 -0700

    Bump mdbook-alerts
    Closes #70
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/mdbook-alerts/default.nix
+++ nix/mdbook-alerts/default.nix
@@ -8,16 +8,16 @@
 rustPlatform.buildRustPackage rec {
   owner = "lambdalisue";
   pname = "rs-mdbook-alerts";
-  version = "0.6.10";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-xyjLrqNb/YC2FWQXsCFJgiIDZz4xWACnrUiBuXS2Nio";
+    hash = "sha256-MZS9TESITj3tzdaXYu5S2QUCW7cZuTpH1skFKeVi/sQ";
   };
 
-  cargoHash = "sha256-kzp1bpP4yHfu0BakN/O5OxdO/tLs/kWNmnaUopIlBVM";
+  cargoHash = "sha256-Icim4+Gcbh1iQZMI9EqaTV5DBSRx4OE9viNsPjq0jTw=";
 
   meta = {
     description = "mdBook preprocessor to add GitHub Flavored Markdown's Alerts to your book";
