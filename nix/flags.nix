93899c84d41bad05ff52d16e3ae9515ff3d06a86
Daniel Noland <daniel@githedgehog.com>
2025-03-09 19:23:27 -0600

    fix: swap mold for lld
    mold is cool because it is fast, but it keeps breaking on more and more builds in our setup.
    
    Easier to just use lld.
    
    Note that rdma-core is known _not_ to compile with lld in the current config.  I will address that in the next patch.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/flags.nix
+++ nix/flags.nix
@@ -3,12 +3,12 @@ rec {
   release = rec {
     CFLAGS = " ${machine} -O3 -ggdb3 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
     CXXFLAGS = CFLAGS;
-    LDFLAGS = "-Wl,-O3 -Wl,--gc-sections -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=1 -Wl,-plugin-opt,jobs=1 -Qunused-arguments";
+    LDFLAGS = "-fuse-ld=lld -Wl,-O3 -Wl,--gc-sections -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=1 -Wl,-plugin-opt,jobs=1 -Qunused-arguments";
   };
 
   dev = rec {
     CFLAGS = " ${machine} -Og -ggdb3 -fno-inline -Qunused-arguments";
     CXXFLAGS = CFLAGS;
-    LDFLAGS = "";
+    LDFLAGS = "-fuse-ld=lld";
   };
 }
