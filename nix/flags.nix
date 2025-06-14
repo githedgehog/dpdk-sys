02a1a461d1ad2413c60710a90c1071b79ed0d3ec
Daniel Noland <daniel@githedgehog.com>
2025-06-13 20:41:53 -0600

    fix(ci): speed up ci build
    We have new runners which don't seem prone to OOM killing our builds when they run more LTO linkers in parallel.
    
    More, we seem to be able to get away with a lot more cores now.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/flags.nix
+++ nix/flags.nix
@@ -3,7 +3,7 @@ rec {
   release = rec {
     CFLAGS = " ${machine} -O3 -ggdb3 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
     CXXFLAGS = CFLAGS;
-    LDFLAGS = "-flto=thin -fuse-ld=lld -Wl,-O3 -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=1 -Qunused-arguments";
+    LDFLAGS = "-flto=thin -fuse-ld=lld -Wl,-O3 -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=6 -Qunused-arguments";
   };
 
   debug = rec {
