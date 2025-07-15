d02f1bd7878da7ad1dee2ba1fb8aefc968da43ed
Daniel Noland <daniel@githedgehog.com>
2025-07-15 11:46:13 -0600

    chore: flags.nix cleanup
    Reduce repetition in flags.nix
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/flags.nix
+++ nix/flags.nix
@@ -1,13 +1,14 @@
 rec {
   machine = "-mcrc32 -mrtm -msse4 -mssse3 -march=x86-64-v3 -mtune=znver4";
+  debug_flags = "-ggdb3 -gdwarf-5";
   release = rec {
-    CFLAGS = " ${machine} -O3 -ggdb3 -gdwarf-5 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
+    CFLAGS = " ${machine} ${debug_flags} -O3 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
     CXXFLAGS = CFLAGS;
     LDFLAGS = "-flto=thin -fuse-ld=lld -Wl,-O3 -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=6 -Qunused-arguments";
   };
 
   debug = rec {
-    CFLAGS = " ${machine} -ggdb3 -gdwarf-5 -Og -fno-inline -Qunused-arguments";
+    CFLAGS = " ${machine} ${debug_flags} -Og -fno-inline -Qunused-arguments";
     CXXFLAGS = CFLAGS;
     LDFLAGS = "-fuse-ld=lld";
   };
