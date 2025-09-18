25a28a39c2129ecec470ad433abfd35b9d77d3f7
Daniel Noland <daniel@githedgehog.com>
2025-09-18 05:23:35 +0000

    feat: embed source code in compile
    Now that we are using dwarf-5 we have the option of embedding the C or
    Rust source code directly into the .a file.  This makes the sysroot a
    bit larger, but is extremely useful for debugging dpdk related
    functionality.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/flags.nix
+++ nix/flags.nix
@@ -1,13 +1,13 @@
 rec {
   machine = "-mcrc32 -mrtm -msse4 -mssse3 -march=x86-64-v3 -mtune=znver4";
   release = rec {
-    CFLAGS = " ${machine} -O3 -ggdb3 -gdwarf-5 -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
+    CFLAGS = " ${machine} -O3 -ggdb3 -gdwarf-5 -gembed-source -gz -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
     CXXFLAGS = CFLAGS;
     LDFLAGS = "-flto=thin -fuse-ld=lld -Wl,-O3 -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=6 -Qunused-arguments";
   };
 
   debug = rec {
-    CFLAGS = " ${machine} -Og -ggdb3 -gdwarf-5 -fno-inline -Qunused-arguments";
+    CFLAGS = " ${machine} -Og -ggdb3 -gdwarf-5 -gembed-source -gz -fno-inline -Qunused-arguments";
     CXXFLAGS = CFLAGS;
     LDFLAGS = "-fuse-ld=lld";
   };
