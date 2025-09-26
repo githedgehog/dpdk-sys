5eae18deba3744653ab487f486812fe18483ceca
Daniel Noland <daniel@githedgehog.com>
2025-09-26 22:53:18 +0000

    chore: reformat flags.nix
    This file was pointlessly messy.
    
    Signed-off-by: Daniel Noland <daniel@githedgehog.com>

--- nix/flags.nix
+++ nix/flags.nix
@@ -1,15 +1,37 @@
-rec {
-  machine = "-mcrc32 -mrtm -msse4 -mssse3 -march=x86-64-v3 -mtune=znver4";
-  release = rec {
-    CFLAGS = " ${machine} -O3 -ggdb3 -gdwarf-5 -gembed-source -gz -flto=thin -Werror=odr -Werror=strict-aliasing -fstack-protector-strong -Qunused-arguments";
-    CXXFLAGS = CFLAGS;
-    LDFLAGS = "-flto=thin -fuse-ld=lld -Wl,-O3 -Wl,-z,relro,-z,now -Wl,--thinlto-jobs=6 -Qunused-arguments";
+let
+  flags = {
+    compile = {
+      machine = "-mcrc32 -mrtm -msse4 -mssse3 -march=x86-64-v3 -mtune=znver4";
+      # note: debug is applied to all profiles: don't include performance compromising flags here.  Add those to the profiles.
+      debug = "-ggdb3 -gdwarf-5 -gembed-source -gz";
+      security = "-fstack-protector-strong";
+      errors = "-Werror=odr -Werror=strict-aliasing";
+      profile = {
+        debug = "-Og -fno-inline -fno-omit-frame-pointer";
+        release = "-O3 -flto=thin";
+      };
+      end = "-Qunused-arguments";
+    };
+    link = {
+      linker = "-fuse-ld=lld";
+      profile = {
+        debug = "";
+        release = "-flto=thin -Wl,-O3 -Wl,-z,relro,-z,now";
+      };
+      end = "-Qunused-arguments";
+    };
   };
-
-  debug = rec {
-    CFLAGS = " ${machine} -Og -ggdb3 -gdwarf-5 -gembed-source -gz -fno-inline -Qunused-arguments";
-    CXXFLAGS = CFLAGS;
-    LDFLAGS = "-fuse-ld=lld";
+  cflags =
+    type: with flags.compile; "${machine} ${debug} ${security} ${errors} ${profile.${type}} ${end}";
+  cxxflags = type: cflags type;
+  ldflags = type: with flags.link; "${linker} ${profile.${type}} ${end}";
+  configuration = type: {
+    CFLAGS = cflags type;
+    CXXFLAGS = cxxflags type;
+    LDFLAGS = ldflags type;
   };
-
+in
+{
+  release = configuration "release";
+  debug = configuration "debug";
 }
